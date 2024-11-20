310d9253f13e689396201c9cd06ecc8f045f34eb
Jorge Granda <jorjorgeandgranda06@gmail.com>
2024-11-20 12:03:26 -0500

    Cambios

--- ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo02.java
+++ ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo02.java
@@ -17,14 +17,16 @@ public class Ejemplo02 {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner entrada = new Scanner(System.in);
-        entrada.useLocale(Locale.US);
+        entrada.useLocale(Locale.US);// es para que todos los decimales se 
+        //ingresen en forma de punto y no con coma
         int limite;
         int contador = 1;
         System.out.println("Ingrese el Límite");
         limite = entrada.nextInt(); 
         while (contador <= limite){
             
-            if (contador%2==0) {
+            if (contador%2!=0) { // este nos ayuda a poder agruparar los pares 
+                //tambien los impares 
                 System.out.printf("Contador %d\n", 
                     contador);
             }
